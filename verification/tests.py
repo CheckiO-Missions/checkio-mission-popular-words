70ef773c8cefb5ec55e0deef988c85135cf15a43
Alexander Viter <engineermexanic@gmail.com>
2018-04-10 18:22:00 +0300

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -13,10 +13,10 @@ Not if I ran.'''
 
 TEXT3 = '''And the Raven, never flitting, still is sitting, still is sitting
 On the pallid bust of Pallas just above my chamber door,
-    And his eyes have all the seeming of a demon’s that is dreaming,
-    And the lamp-light o’er him streaming throws his shadow on the floor,
+And his eyes have all the seeming of a demon’s that is dreaming,
+And the lamp-light o’er him streaming throws his shadow on the floor,
 And my soul from out that shadow that lies floating on the floor
-            Shall be lifted nevermore!'''
+Shall be lifted nevermore!'''
 
 TEXT4 = '''Shall I go to the cinema?
 No, you shouldn't!'''
@@ -24,7 +24,7 @@ No, you shouldn't!'''
 TESTS = {
     "Basics": [
         {
-            "input": [TEXT, ['i', 'was', 'three']],
+            "input": [TEXT, ['i', 'was', 'three', 'o']],
             "answer": {
                 'i': 4,
                 'was': 3,
@@ -43,12 +43,13 @@ TESTS = {
             }
         },
         {
-            "input": [TEXT2, ["it's", "ran"]],
+            "input": [TEXT2, ["it's", "ran", "i"]],
             "answer": {
                 "it's": 1,
                 "ran": 1,
-                'i': 2
-            },
+                "i": 2
+            }
+        },
         {
             "input": [TEXT3, ["raven", "still", "is", "floor", "nevermore"]],
             "answer": {
@@ -57,7 +58,8 @@ TESTS = {
                 'is': 3,
                 'floor': 2,
                 'nevermore': 1
-            },
+            }
+        },
         {
             "input": [TEXT4, ["i", "you", "o"]],
             "answer": {
